6a29f7ee8343c0f1889545ceeff123781023628a
Yu-Linbo <326282940@qq.com>
2022-05-20 20:42:56 +0800

    frist commit

---
+++ cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ylb/Develop/ORB_SLAM2/src/Converter.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Converter.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/ExtractLineSegment.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/ExtractLineSegment.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/Frame.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Frame.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/FrameDrawer.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/FrameDrawer.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/Initializer.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Initializer.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/KeyFrame.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/KeyFrame.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/KeyFrameDatabase.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/KeyFrameDatabase.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/LSDmatcher.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/LSDmatcher.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/LocalMapping.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/LocalMapping.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/LoopClosing.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/LoopClosing.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/Map.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Map.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/MapDrawer.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/MapDrawer.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/MapLine.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/MapLine.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/MapPoint.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/MapPoint.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/ORBextractor.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/ORBextractor.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/ORBmatcher.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/ORBmatcher.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/Optimizer.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Optimizer.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/PnPsolver.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/PnPsolver.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/Sim3Solver.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Sim3Solver.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/System.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/System.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/Tracking.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Tracking.cc.o"
+  "/home/ylb/Develop/ORB_SLAM2/src/Viewer.cc" "/home/ylb/Develop/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  "ORB_SLAM2_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/local/include/eigen3"
+  "/usr/local/lib/cmake/Pangolin/../../../include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
